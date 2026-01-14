33ffb1fa69510737109ad793807c81fab1f13d82
Jeanpierremrtnz <jm4581915@gmail.com>
2026-01-14 10:35:40 -0500

    validaciones

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,13 +3,15 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     /**
-     * Define the application's command schedule.
+     * Define the application's schedule.
      */
+
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
